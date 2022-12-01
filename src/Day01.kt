73947d9f62c769abfa13a5ae53e670e9eaae8671
Miloslav G. Milenkov <mgmilenkov@gmail.com>
2022-12-01 18:58:04 +0000

    Day1 pt.2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,13 +1,26 @@
 import java.io.File
 
 fun main() {
-    fun calculateMax(input: String): Int  = input.split("\n\n")
+    fun calculateMax(input: String): Int?  =
+        input.split("\n\n")
+            .maxOfOrNull { elf ->
+                elf.lines()
+                    .sumOf {
+                        it.toInt()
+                    }
+    }
+
+    fun calculateSumOfThree(input: String): Int  =
+        input.split("\n\n")
             .map { elf ->
                 elf.lines()
                     .sumOf {
                         it.toInt()
                     }
-            }.max()
+            }.sortedDescending()
+            .subList(0,3)
+            .sum()
 
     println(calculateMax(input = readText("day1")))
+    println(calculateSumOfThree(input = readText("day1")))
 }
