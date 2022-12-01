06d08f2af4c20532a3122e22425cf79035e16c6a
Miloslav G. Milenkov <mgmilenkov@gmail.com>
2022-12-01 18:49:24 +0000

    Day1 pt1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,13 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun calculateMax(input: String): Int  = input.split("\n\n")
+            .map { elf ->
+                elf.lines()
+                    .sumOf {
+                        it.toInt()
+                    }
+            }.max()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(calculateMax(input = readText("day1")))
 }
